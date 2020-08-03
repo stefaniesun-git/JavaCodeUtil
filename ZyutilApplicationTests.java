362f2066f4d8b973ccc13fc181bdd7cced585472
stefaniesun-git <66008865+stefaniesun-git@users.noreply.github.com>
2020-08-03 17:58:00 +0800

    Add files via upload

---
+++ ZyutilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.zyutil;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZyutilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
